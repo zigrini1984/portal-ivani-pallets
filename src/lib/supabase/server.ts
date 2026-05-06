820b9d7537ff507bca862d0d7ae6f79c9ef2269b
Zigrini <ronaldotrettel@gmail.com>
2026-05-05 22:42:16 -0300

    fix: simplificar inicializacao do supabase client no server

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -6,12 +6,6 @@ export function createClientServer() {
 
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
-    {
-      auth: {
-        persistSession: false,
-        autoRefreshToken: false,
-      },
-    }
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
   );
 }
