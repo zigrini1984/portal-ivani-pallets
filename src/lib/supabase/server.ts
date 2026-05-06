42f3ceb940f4cb9e13634dce126ca6a82dea0edf
Zigrini <ronaldotrettel@gmail.com>
2026-05-05 22:40:50 -0300

    debug: adicionar logs de env no supabase server

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -1,6 +1,9 @@
 import { createClient } from "@supabase/supabase-js";
 
 export function createClientServer() {
+  console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+  console.log("SUPABASE KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.slice(0, 20));
+
   return createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
