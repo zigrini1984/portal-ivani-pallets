4119b304674c773ad36534b6303e775cf2cf4f61
Zigrini <ronaldotrettel@gmail.com>
2026-05-05 21:39:32 -0300

    chore: adicionar logs de env no server.ts para debug na vercel

--- src/lib/supabase/server.ts
+++ src/lib/supabase/server.ts
@@ -4,6 +4,9 @@ import { cookies } from "next/headers";
 export async function createClient() {
   const cookieStore = await cookies();
 
+  console.log("SUPABASE URL:", process.env.NEXT_PUBLIC_SUPABASE_URL);
+  console.log("SUPABASE KEY:", process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY?.slice(0,20));
+
   return createServerClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL!,
     process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
